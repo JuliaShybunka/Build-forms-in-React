8ffa4855ebb64ea9497f7043e9c295ef3a8a7829
JuliaShybunka <julishybunka@gmail.com>
2020-01-31 11:29:48 +0200

    Update InputForm component

--- src/InputForm.js
+++ src/InputForm.js
@@ -2,28 +2,87 @@ import React from "react";
 
 class InputForm extends React.Component {
   state = {
-    name: ""
+    username: "",
+    password: "",
+    selectedProduct: "",
+    check: ""
   };
 
   handleChange = event => {
-    this.setState({ name: event.target.value });
-    console.log(event.target.value);
+    const name = event.target.name;
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleSelect = event => {
+    this.setState({ selectedProduct: event.target.value });
+  };
+
+  handleCheck = event => {
+    this.setState({ check: event.target.checked });
   };
 
   handleSubmit = event => {
+    const state = this.state;
+    alert(
+      "Log in form, " +
+        state.username +
+        ", " +
+        state.password +
+        ", " +
+        state.selectedProduct +
+        ", " +
+        state.check +
+        "."
+    );
     event.preventDefault();
   };
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" name="text" onChange={this.handleChange} />
-          <input type="password" />
-          <select>
-            <option>1</option>
-            <option>2</option>
-          </select>
-          <input type="checkbox"></input>
+          <label>
+            Username:
+            <input
+              value={this.state.username}
+              type="text"
+              name="username"
+              onChange={this.handleChange}
+            />
+            <p>{this.state.username}</p>
+          </label>
+          <label>
+            Password:
+            <input
+              value={this.state.password}
+              type="password"
+              name="password"
+              onChange={this.handleChange}
+            />
+            <p>{this.state.password}</p>
+          </label>
+          <label>
+            Select a product
+            <select
+              name="selectedProduct"
+              value={this.state.selectedProduct}
+              onChange={this.handleSelect}
+            >
+              <option value="--">--</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
+            <p> {this.state.selectedProduct}</p>
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              value={this.state.check}
+              name="checkbox"
+              onChange={this.handleCheck}
+            />
+            Keep me logged in
+          </label>
+
           <button type="submit">Log in</button>
         </form>
       </div>
